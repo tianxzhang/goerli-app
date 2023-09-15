615f0397b0f60b6e788a732c5727a74bbab67c55
tianxiang <tianxzhang@163.com>
2023-09-15 18:15:43 +0800

    solve cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://eth-goerli.alchemyapi.io/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
